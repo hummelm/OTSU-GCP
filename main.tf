e4f053c63fe2d719f6a05efdf177f0840f3780bc
hummelm <hummel.michel@gmail.com>
2026-02-01 16:49:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 # Configuration du fournisseur Google Cloud
 provider "google" {
-  project = "votre-project-id"
+  project = "otsu-2026"
   region  = "europe-west1"
 }
 
 # --- RESEAU ---
-
 resource "google_compute_network" "vpc_network" {
   name                    = "vpc-cours-informatique"
   auto_create_subnetworks = false
@@ -32,11 +31,11 @@ resource "google_compute_firewall" "allow_ssh_http" {
 }
 
 # --- INSTANCE WEB & LOAD BALANCER ---
-
 resource "google_compute_instance" "web_instance" {
-  name         = "web-server"
-  machine_type = "e2-medium"
+  name         = "web-server-${count.index}"
+  machine_type = "e2-micro"
   zone         = "europe-west1-b"
+  count        = 2
 
   boot_disk {
     initialize_params {
@@ -49,14 +48,39 @@ resource "google_compute_instance" "web_instance" {
     access_config {}
   }
 
-  metadata_startup_script = "apt-get update && apt-get install -y apache2"
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    apt-get update
+    apt-get install -y nginx
+    echo '<!DOCTYPE html>
+<html>
+<head>
+<title>Full AUTO nginx ${count.index}!</title>
+<style>
+html { color-scheme: light dark; }
+body { width: 35em; margin: 0 auto;
+font-family: Tahoma, Verdana, Arial, sans-serif; }
+</style>
+</head>
+<body>
+<h1>Full AUTO nginx ${count.index}</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>'>/var/www/html/index.nginx-debian.html
+    EOT
 }
 
 # Groupe d'instances pour le Load Balancer
 resource "google_compute_instance_group" "web_group" {
   name = "web-server-group"
   zone = "europe-west1-b"
-  instances = [google_compute_instance.web_instance.id]
+  instances = google_compute_instance.web_instance[*].id
   named_port {
     name = "http"
     port = 80
@@ -64,7 +88,6 @@ resource "google_compute_instance_group" "web_group" {
 }
 
 # --- COMPOSANTS DU LOAD BALANCER HTTP ---
-
 resource "google_compute_global_forwarding_rule" "default" {
   name       = "web-forwarding-rule"
   target     = google_compute_target_http_proxy.default.id
@@ -99,4 +122,13 @@ resource "google_compute_health_check" "default" {
   http_health_check {
     port = 80
   }
+}
+output "load_balancer_public_ip" {
+  description = "L'adresse IP publique du Load Balancer pour accéder à l'application"
+  value       = google_compute_global_forwarding_rule.default.ip_address
+}
+
+output "web_servers_public_ips" {
+  description = "Les adresses IP publiques des serveurs web"
+  value       = google_compute_instance.web_instance[*].network_interface[0].access_config[0].nat_ip
 }
